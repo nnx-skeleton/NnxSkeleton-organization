9770818b1be5d88cb3ab95c9994085caaeb35399
dmitry <plitakov@platformasoft.ru>
2016-04-29 13:44:04 +0300

    add config files for doctrine

--- config/doctrine.config.php
+++ config/doctrine.config.php
@@ -5,7 +5,17 @@ namespace NnxMember\Organization;
 return [
     'doctrine' => [
         'driver' => [
-            'NnxMemberOrganizationEntity' => [
+            'NnxMemberOrganizationEntityMember' => [
+                'cache' => 'array',
+                'paths' => [__DIR__ . '/../src/Entity'],
+                'class' => \Doctrine\ORM\Mapping\Driver\AnnotationDriver::class,
+            ],
+            'NnxMemberOrganizationEntityOrganization' => [
+                'cache' => 'array',
+                'paths' => [__DIR__ . '/../src/Entity'],
+                'class' => \Doctrine\ORM\Mapping\Driver\AnnotationDriver::class,
+            ],
+            'NnxMemberOrganizationEntityUser' => [
                 'cache' => 'array',
                 'paths' => [__DIR__ . '/../src/Entity'],
                 'class' => \Doctrine\ORM\Mapping\Driver\AnnotationDriver::class,
