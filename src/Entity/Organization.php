01e52cbf00181dcd53f37a9ee1b4f3ed9fc825b5
nnx-skelet <nnx-skeleton@yandex.ru>
2016-05-04 12:13:06 +0300

    Update Organization.php

--- src/Entity/Organization.php
+++ src/Entity/Organization.php
@@ -5,13 +5,13 @@
  * Date: 27.04.16
  * Time: 14:22
  */
-namespace NnxMember\Organization\Entity;
+namespace NnxSkeleton\Organization\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
 
 /**
  * Class Organization
- * @package NnxMember\Organization\Entity
+ * @package NnxSkeleton\Organization\Entity
  *
  * @ORM\Entity()
  * @ORM\Table(name="organization")
