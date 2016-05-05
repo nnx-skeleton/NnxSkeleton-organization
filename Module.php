7805c2523995590d6fb44888c2d1fead1c0c6ac7
dmitry <plitakov@platformasoft.ru>
2016-05-05 09:03:35 +0300

    change module.config.php files

--- Module.php
+++ Module.php
@@ -6,6 +6,40 @@
  * Time: 15:38
  */
 
-class Module {
+use Zend\ModuleManager\Feature\AutoloaderProviderInterface;
+use Zend\ModuleManager\Feature\ConfigProviderInterface;
 
+class Module implements
+    AutoloaderProviderInterface,
+    ConfigProviderInterface
+{
+
+    /**
+     * Имя модуля
+     *
+     * @var string
+     */
+    const MODULE_NAME = __NAMESPACE__;
+
+    /**
+     * @return array
+     */
+    public function getAutoloaderConfig()
+    {
+        return array(
+            'Zend\Loader\StandardAutoloader' => array(
+                'namespaces' => array(
+                    __NAMESPACE__ => __DIR__ . '/src/',
+                ),
+            ),
+        );
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getConfig()
+    {
+        return include __DIR__ . '/config/module.config.php';
+    }
 } 
---
+++ config/module.config.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: dmitry
+ * Date: 29.04.16
+ * Time: 12:32
+ */
+namespace NnxSkeleton\Organization;
+
+$config = [
+
+];
+
+return array_merge_recursive(
+    include __DIR__ . '/doctrine.config.php',
+    $config
+);
